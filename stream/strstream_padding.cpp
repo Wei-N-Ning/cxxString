1ef873ab6913f611f357f6b726129b7c430a28a9
Wei Ning <wning@wetafx.co.nz>
2018-08-15 13:05:43 +1200

    - add left/right adjustified example using io manipulator

---
+++ stream/strstream_padding.cpp
@@ -0,0 +1,24 @@
+
+#include <iomanip>
+#include <sstream>
+#include <cassert>
+
+using namespace std;
+
+void left_adjustified() {
+    stringstream ss;
+    ss << left << setfill(' ') << setw(7) << "12345";
+    assert(string("12345  ") == ss.str());
+}
+
+void right_adjustified() {
+    stringstream ss;
+    ss << right << setfill('.') << setw(8) << "4123";
+    assert(string("....4123") == ss.str());
+}
+
+int main() {
+    left_adjustified();
+    right_adjustified();
+    return 0;
+}
