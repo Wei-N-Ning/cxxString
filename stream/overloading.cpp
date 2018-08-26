fd663661d20bfc4866747fc40439f2ae76299bb9
Wei Ning <nwcgnw@gmail.com>
2018-08-26 15:35:39 +1200

    - add example about stream operator overloading in namespaces

---
+++ stream/overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// source
+// https://stackoverflow.com/questions/476272/how-to-properly-overload-the-operator-for-an-ostream
+
+namespace FooNS {
+    struct Foo {};
+
+    std::ostream& operator<< (std::ostream& os, const Foo& foo) {
+        os << "Inside namespace{Foo}";
+        return os;
+    }
+} // namespace Foo
+
+int main() {
+    // overload the ostream operator in the global namespace is a BAD
+    // idea and will not compile in this case;
+    // compiler throws an error about ambiguous overload
+    
+    // 1) use a namespace to encapsulate the class
+    // 2) define the overloaded stream operator in that namespace
+
+    FooNS::Foo foo;
+    std::cout << foo << std::endl;;
+    return 0;
+}
