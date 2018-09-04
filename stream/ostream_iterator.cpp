e1d90fa708a906630f241cce66db33a9f1c67bb3
Wei Ning <nwcgnw@gmail.com>
2018-09-05 10:34:46 +1200

    - update ostream iterator example

--- stream/ostream_iterator.cpp
+++ stream/ostream_iterator.cpp
@@ -7,16 +7,81 @@
 // The problem is that the delimiter in the ostream_iterator constructor
 // call is better described as a suffix than a delimiter.
 
+// while using universal reference, how can one use T::value_type as
+// a parameter now that T will also be a reference?
+// see:
+// https://stackoverflow.com/questions/35407243/using-a-class-template-in-a-function-template-with-universal-reference-parameter
+// key: std::remove_reference<T>::type
+
+// if the value_type of the container is of a user-defined class, there
+// must an overloaded ostream operator<< for this type
+
 #include <algorithm>
 #include <vector>
 #include <iterator>
 #include <iostream>
+#include <type_traits>
+
+template <class T>
+void prt(T &&container, const char *delimit, const char *open__ = nullptr,
+         const char *close__ = nullptr) {
+  if (!container.empty()) {
+    if (!open__) {
+      open__ = "(";
+    }
+    if (!close__) {
+      close__ = ")";
+    }
+    std::cout << open__;
+    std::copy(container.cbegin(), container.cend() - 1,
+              std::ostream_iterator<
+                  typename std::remove_reference<T>::type::value_type>(
+                  std::cout, delimit));
+    std::copy(container.cend() - 1, container.cend(),
+              std::ostream_iterator<
+                  typename std::remove_reference<T>::type::value_type>(
+                  std::cout, ""));
+    std::cout << close__;
+  }
+}
+
+void test_empty() {
+    prt(std::vector<int>{}, "", "(", ")\n");
+}
+
+void test_lvalue_ref() {
+    auto a = std::vector<double>{1.010, 2.13, 142.12};
+    prt(a, ", ");
+}
+
+void test_rvalaue_ref() {
+    prt(std::vector<double>{1.010, 2.13, 142.12}, ", ");
+}
+
+namespace CustomType {
+
+class A {
+public:
+    char label = 'A';
+};
+
+std::ostream& operator<< (std::ostream& os, const A& a) {
+    os << a.label;
+    return os;
+}
+
+}
+
+
+void test_custom_type() {
+    prt(std::vector<CustomType::A>(10), "");
+}
+
 
 int main() {
-    std::vector<int> sut{1, 3, 3, 1};
-    std::cout << "(";
-    std::copy(sut.cbegin(), sut.cend(),
-        std::ostream_iterator<int>(std::cout, ", "));
-    std::cout << ")";
+    test_empty();
+    test_lvalue_ref();
+    test_rvalaue_ref();
+    test_custom_type();
     return 0;
 }
