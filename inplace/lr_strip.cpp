932bfde2128c4108e6002ef43a7093d705b83986
Wei Ning <nwcgnw@gmail.com>
2018-09-19 19:03:53 +1200

    add python-style strip() implementation

---
+++ inplace/lr_strip.cpp
@@ -0,0 +1,87 @@
+
+#include <sstream>
+#include <vector>
+#include <cassert>
+
+// source
+// http://www.martinbroadhurst.com/how-to-trim-a-stdstring.html
+// see also:
+// http://www.cplusplus.com/reference/string/string/erase/
+
+void RunTinyTests();
+
+void test_lstrip() {
+    auto f = [](std::string& o_str) {
+        o_str.erase(0, o_str.find_first_not_of("\t\n\v\f\r "));
+    };
+    
+    // note that the second row is ' iddqd ' (trailing space)
+    std::string sut = R"IDDQD(
+ iddqd 
+ther
+ 2 1
+)IDDQD";
+    std::stringstream ss(sut);
+    std::string line;
+    std::vector<std::string> results;
+    while (std::getline(ss, line).good()) {
+        f(line);
+        results.emplace_back(std::move(line));
+    }
+   
+    std::vector<std::string> expected{"", "iddqd ", "ther", "2 1"};
+    assert(expected == results);
+}
+
+void test_rstrip() {
+    auto f = [](std::string& o_str) {
+        o_str.erase(o_str.find_last_not_of("\t\n\v\f\r ") + 1);
+    };
+    
+    // each non-empty line has some kind of whitespace characters
+    std::string sut = R"IDDQD(
+ iddqd  
+ther      
+ 2 1   
+)IDDQD";
+
+    std::stringstream ss(sut);
+    std::string line;
+    std::vector<std::string> results;
+    while (std::getline(ss, line).good()) {
+        f(line);
+        results.emplace_back(std::move(line));
+    }
+    
+    std::vector<std::string> expected{"", " iddqd", "ther", " 2 1"};
+    assert(expected == results);
+}
+
+void test_strip() {
+    auto f = [](std::string& o_str) {
+        o_str.erase(0, o_str.find_first_not_of("\t\n\v\f\r "));
+        o_str.erase(o_str.find_last_not_of("\t\n\v\f\r ") + 1);
+    };
+
+    std::string sut = R"IDDQD(
+ idd qd         
+   ther   
+ 2 1  
+)IDDQD";
+
+    std::stringstream ss(sut);
+    std::string line;
+    std::vector<std::string> results;
+    while (std::getline(ss, line).good()) {
+        f(line);
+        results.emplace_back(std::move(line));
+    }
+    
+    std::vector<std::string> expected{"", "idd qd", "ther", "2 1"};
+    assert(expected == results);
+}
+
+int main() {
+    RunTinyTests();
+    return 0;
+}
