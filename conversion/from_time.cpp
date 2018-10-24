fcf7338ca269ae9dcd412874d1c3cdf9c168c980
Wei Ning <wning@wetafx.co.nz>
2018-10-24 17:56:28 +1300

    update ctest

--- conversion/from_time.cpp
+++ conversion/from_time.cpp
@@ -3,10 +3,16 @@
 #include <string>
 #include <chrono>
 
-int main() {
+void RunTinyTests();
+
+void test_from_nanoseconds_to_uint64() {
     uint64_t now = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); 
     std::string s = std::to_string(now);
     std::cout << now << " -> " << s << std::endl;
-    
+}
+
+int main() {
+    RunTinyTests();
+
     return 0;
 }
