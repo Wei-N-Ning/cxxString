b547848d1503cc427de024f6885a4233058fc5c5
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-04-14 09:44:01 +1000

    working on modern c++ challenges

--- conversion/from_numeric.cpp
+++ conversion/from_numeric.cpp
@@ -1,16 +1,19 @@
 
 // exceptional Chapter 2, P25
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest/doctest.h"
 
 #include <iomanip>
-#include <sstream>
+#include <algorithm>
+#include <string>
+#include <iterator>
 
 #include <cstdio>
 #include <cstring>
 #include <cassert>
 
-TEST_CASE("sprintf caveat") {
+TEST_CASE ("sprintf caveat") {
     char smallBuf[5] = {0, 0, 0, 0, 0};
 
     // given a char array of length 5
@@ -29,7 +32,7 @@ TEST_CASE("sprintf caveat") {
     // immediate;y after smallBuf in memory
 }
 
-TEST_CASE("string-stream") {
+TEST_CASE ("string-stream") {
     std::stringstream ss;
     ss << std::setw(4) << 1000000;
     assert(std::string("1000000") == ss.str());
@@ -39,7 +42,38 @@ TEST_CASE("string-stream") {
 
 }
 
-TEST_CASE("from int to string") {
+TEST_CASE ("from int to string") {
     std::string s = std::to_string(1337);
     assert(s == std::string("1337"));
+}
+
+// source: modern c++ challenge P/51
+std::vector<unsigned char> hexstring_to_vec(const std::string &s) {
+    using namespace std;
+
+    vector<unsigned char> temp(s.size());
+    transform(s.cbegin(), s.cend(), temp.rbegin(),
+              [](const auto &ch) -> unsigned char {
+                  if (ch >= '0' && ch <= '9') return ch - '0';
+                  if (ch >= 'A' && ch <= 'F') return ch - 'A' + 10;  // 8,9,a,b...
+                  if (ch >= 'a' && ch <= 'f') return ch - 'a' + 10;
+                  throw invalid_argument("Invalid hexstring");
+              });
+    if (s.size() % 2 == 1) {
+        temp.push_back(0);
+    }
+    vector<unsigned char> result(temp.size() / 2);
+    auto src_it = temp.rbegin();
+    for (auto &elem : result) {
+        elem = 16 * *src_it + *(++src_it);  // end here -> 0xd, 0xa, 0xb, 0x0 <-- start here
+        ++src_it;
+    }
+    return result;
+};
+TEST_CASE ("string to 8-bit ints") {
+    using namespace std;
+    CHECK_EQ(vector<unsigned char>{0x0B, 0xAD}, hexstring_to_vec("BAD"));
+    CHECK_EQ(vector<unsigned char>{0xBA, 0xAD}, hexstring_to_vec("BAAD"));
+    CHECK_EQ(vector<unsigned char>{0x0B, 0xAD, 0xF0, 0x0D}, hexstring_to_vec("BADF00D"));
+    CHECK_EQ(vector<unsigned char>{1, 2, 3, 4, 5, 6}, hexstring_to_vec("010203040506"));
 }
