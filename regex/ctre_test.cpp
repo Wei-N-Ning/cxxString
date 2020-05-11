35e875ad81cd0baed6d12acfe42b5e1bf5b7634e
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-12 09:59:18 +1000

    ctre

---
+++ regex/ctre_test.cpp
@@ -0,0 +1,19 @@
+//
+// Created by weining on 12/5/20.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+#include "ctre.hpp"
+
+#include <string>
+#include <iostream>
+
+TEST_CASE ("") {
+    using namespace std;
+    using namespace string_literals;
+
+    auto s{R"RE(there is a cow)RE"s};
+}
