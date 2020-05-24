5aabce20fee4bacd53ccad681b88dd8cdb105d3d
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-24 20:29:23 +1000

    more on formating

---
+++ formatting/printf.cpp
@@ -0,0 +1,7 @@
+// good old printf 
+// it can be useful in a multi-threading situation, for quickly
+// dumping some text to stdout
+// format:
+// https://stackoverflow.com/questions/3209909/how-to-printf-unsigned-long-in-c
+//
+// %llu, %lu,
