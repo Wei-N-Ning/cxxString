4c71f8139707c4fa0cbd422d0891375b4c001754
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-06-06 18:07:40 +1000

    print raw bytes

--- formatting/README.md
+++ formatting/README.md
@@ -1,3 +1,7 @@
 # String (stream) formatting
 
 like python's str.format()
+
+## Print raw bytes
+
+see print_raw_bytes.cpp - a cheap memory inspection tool
---
+++ formatting/print_raw_bytes.cpp
@@ -0,0 +1,21 @@
+//
+// Created by weining on 6/6/20.
+//
+
+// source:
+// https://stackoverflow.com/questions/10599068/how-do-i-print-bytes-as-hexadecimal
+
+void print_bytes() {
+    std::byte stackBuf[128];
+
+    using namespace std;
+    cout << hex << setfill('0');  // needs to be set only once
+    auto *ptr = reinterpret_cast<unsigned char *>(stackBuf);
+    for (int i = 0; i < 64; i++, ptr++) {
+        if (i && i % 32 == 0) {
+            cout << endl;
+        }
+        cout << setw(2) << static_cast<unsigned>(*ptr) << " ";
+    }
+    cout << endl;
+}
