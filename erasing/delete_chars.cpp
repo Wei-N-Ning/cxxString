eaa99bfd2d90aa954c9a2b2b3f262331f6c14fb5
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-06-08 13:36:57 +1000

    more boost string

---
+++ erasing/delete_chars.cpp
@@ -0,0 +1,14 @@
+//
+// Created by weining on 8/6/20.
+//
+
+// c++ boost app dev P/213
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include <boost/algorithm/string/erase.hpp>
+
+TEST_CASE ("") {
+    ;
+}
--- formatting/printf.cpp
+++ formatting/printf.cpp
@@ -1,7 +1,35 @@
-// good old printf 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include <boost/format.hpp>
+#include <string>
+#include <iostream>
+
+// good old printf
 // it can be useful in a multi-threading situation, for quickly
 // dumping some text to stdout
 // format:
 // https://stackoverflow.com/questions/3209909/how-to-printf-unsigned-long-in-c
 //
+// for unsigned long/long type
 // %llu, %lu,
+
+// c++ boost app dev P/211
+struct Item {
+    std::size_t id{};
+    std::string name{};
+    int value{};
+
+    [[nodiscard]] std::string to_str(const std::string &f_) const {
+        boost::format f(f_);
+        unsigned char flags = boost::io::all_error_bits;
+        flags ^= boost::io::too_many_args_bit;
+        f.exceptions(flags);
+        return (f % id % name % value).str();
+    }
+};
+
+TEST_CASE ("use printf format tokens") {
+    Item i{123, "woodhead", 3};
+    std::cout << i.to_str("id: %llu, there is, a cow named %s, with value %i") << std::endl;
+}
