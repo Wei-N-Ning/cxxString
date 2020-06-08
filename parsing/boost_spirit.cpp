4c04f5e29aea5adb08dadcb380b6c2b657b666c0
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-06-08 11:14:07 +1000

    more spirit parsing

--- parsing/boost_spirit.cpp
+++ parsing/boost_spirit.cpp
@@ -8,6 +8,7 @@
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_bind.hpp>
 #include <string>
 #include <optional>
 
@@ -17,6 +18,7 @@
 // NOTE:
 // worth reading into this framework!
 // it has a similar touch and feel to Haskell's parsec
+// this the second example for branching
 // TODO: impl a ini parser in both qi and parsec, compare performance
 
 struct Date {
@@ -97,6 +99,109 @@ TEST_CASE ("parse date string") {
     }
     if (auto o = parse_s("2020-05-18"s); o.has_value()) {
         CHECK_EQ(o->year, 2020);
+    } else {
+        CHECK(false);
+    }
+}
+
+// c++ boost app dev P/77
+enum class ZoneOffsetType {
+    OffsetNotSet,
+    OffsetZ,
+    OffsetUtcPlus,
+    OffsetUtsMinus,
+};
+
+struct DateTime {
+    unsigned short year;
+    unsigned short month;
+    unsigned short day;
+    unsigned short hours;
+    unsigned short minutes;
+    unsigned short seconds;
+    ZoneOffsetType zot;
+    unsigned int zo_in_minutes;
+
+    static void set_zone_offset(
+        DateTime &dt,
+        char sign,  // _1
+        unsigned short hours,  // _2
+        unsigned short minutes  // _3
+    ) {
+        if (sign == '+') {
+            dt.zot = ZoneOffsetType::OffsetUtcPlus;
+        }
+    }
+
+    static void set_zone_offset_type(
+        DateTime &dt,
+        ZoneOffsetType zot
+    ) {
+        dt.zot = zot;
+    }
+};
+
+std::optional<DateTime> parse_dt(const std::string &s) {
+    using boost::spirit::qi::_1;
+    using boost::spirit::qi::_2;
+    using boost::spirit::qi::_3;
+    using boost::spirit::qi::uint_parser;
+    using boost::spirit::qi::char_;
+    using boost::phoenix::bind;
+    using boost::phoenix::ref;
+
+    DateTime dt{};
+
+    uint_parser<unsigned short, 10, 2, 2> u2_;
+    uint_parser<unsigned short, 10, 4, 4> u4_;
+    boost::spirit::qi::rule<const char*, void()> timezone_parser =
+        -(
+            char_('Z')[bind(&DateTime::set_zone_offset_type, ref(dt), ZoneOffsetType::OffsetZ)]
+            |
+            ((char_('+') | char_('-')) >> u2_ >> ':' >> u2_)[bind(&DateTime::set_zone_offset, ref(dt), _1, _2, _3)]
+        );
+
+    boost::spirit::qi::rule<const char*, void()> date_parser =
+        u4_[ref(dt.year) = _1] >> char_('-')
+        >> u2_[ref(dt.month) = _1] >> char_('-')
+        >> u2_[ref(dt.day) = _1];
+
+    boost::spirit::qi::rule<const char*, void()> time_parser =
+        u2_[ref(dt.hours) = _1] >> char_(':')
+        >> u2_[ref(dt.minutes) = _1] >> char_(':')
+        >> u2_[ref(dt.seconds) = _1];
+
+    const char* first = s.data();
+    const char* end = first + s.size();
+
+    bool success = boost::spirit::qi::parse(
+        first, end,
+        (
+                (
+                    date_parser >> char_('T') >> time_parser
+                    | date_parser
+                    | time_parser
+                )
+                >> timezone_parser
+        )
+    );
+    return success ? std::optional<DateTime>{dt} : std::optional<DateTime>{};
+}
+
+TEST_CASE ("parse date-time string with timezone info") {
+    if (auto o = parse_dt("2020-02-12T12:12:12Z"); o.has_value()) {
+        auto v = o.value();
+        CHECK_EQ(v.year, 2020);
+        CHECK_EQ(v.hours, 12);
+        CHECK_EQ(v.zot, ZoneOffsetType::OffsetZ);
+    } else {
+        CHECK(false);
+    }
+
+    if (auto o = parse_dt("2020-02-12T13:23:45+09:30"); o.has_value()) {
+        auto v = o.value();
+        CHECK_EQ(v.year, 2020);
+        CHECK_EQ(v.zot, ZoneOffsetType::OffsetUtcPlus);
     } else {
         CHECK(false);
     }
