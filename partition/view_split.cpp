7d01fc23c280b9f3707497fdbc11a1a132567bff
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-15 20:48:02 +1100

    more notes regarding C++17/20 and FP style

---
+++ partition/view_split.cpp
@@ -0,0 +1,10 @@
+//
+// Created by weining on 15/12/20.
+//
+
+// range-v3 and c++20 ranges api both offer a cleaner (but not necessarily faster) way of splitting
+// string, i.e. tokenization.
+// it is based on the range view and its split() algorithm;
+
+// see cxxFP/data_structure/ranges
+//
